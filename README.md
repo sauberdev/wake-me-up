c43e41ed558fad4d2a4f88e11d01c21a1cdf7361
sauberdev <viacheslav.lylin@gmail.com>
2022-01-16 15:19:29 +0100

    wake-me-up: Implemented first working version
    * If you are moving around light sensor it would wake up device
    * Service stops working when device go to sleep

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wake-me-up
-A small android background service to wake up the device when you are near to it
+## Introduction
+A small android background service to wake up the device when you are near to it. It will wake up the
+device when you are moving around it using data provided by light sensor as alternative for devices
+which doesn't have a front camera for motion detection.
+
+## Limitations
+- It was tested on emulated device and android tablet with a version 7.0
--- app/src/main/java/com/wakemeup/MainActivity.java
+++ app/src/main/java/com/wakemeup/MainActivity.java
@@ -1,14 +1,24 @@
 package com.wakemeup;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
+import android.os.PowerManager;
 import android.util.Log;
-import android.widget.Toast;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mLightSensor;
+    float mPreviousValue = -1;
+    private PowerManager.WakeLock mFullWakeLock;
+
+    final static float VALUE_DIFFERENCE_TO_ACTIVATE_DISPLAY = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +26,62 @@ public class MainActivity extends Activity {
 
         Log.d("MainActivity", "MainActivity started");
 
-        Intent intent = new Intent(this, WakeMeUpService.class);
-        startService(intent);
+        // Initialize sensor
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(this, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-        finish();
+        // Prepare wake lock
+        initWakeLock();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume called");
+    }
+
+    /**
+     * @brief Initializes wake lock to use it later to wake up the device
+     */
+    protected void initWakeLock() {
+        PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mFullWakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "tag:test");
+    }
+
+    /**
+     * @brief Turns on display using wake lock.
+     */
+    private void turnOnDisplay() {
+        Log.d("MainActivity", "Turning on the display");
+
+        // Acquire lock and release it immediately as we want to turn on it only for a minute
+        mFullWakeLock.acquire();
+        mFullWakeLock.release();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d("LightSensor", "Value has changed to " + sensorEvent.values[0]);
+
+        if (mPreviousValue >= 0)
+        {
+            if (Math.abs(mPreviousValue - sensorEvent.values[0]) > VALUE_DIFFERENCE_TO_ACTIVATE_DISPLAY) {
+                turnOnDisplay();
+            }
+        }
+
+        mPreviousValue = sensorEvent.values[0];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
